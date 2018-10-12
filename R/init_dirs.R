064a0b8b510502a615eeb0c86ded5cfd9a634323
bhuvad <bhuva.d@wehi.edu.au>
2018-10-12 12:02:35 +1100

    Initialise restools project. Current progress includes: initialise file structure, initialise markdown docs for plot_diary, lit_review and README for file structure. update_readme coded

---
+++ R/init_dirs.R
@@ -0,0 +1,73 @@
+#' Create directory structure for a data science project
+#'
+#' Initialise a data science project. This function creates a directory
+#' structure to help you organise your project. The structure is inspired from
+#' JEFFERY LEEKS TUTORIAL. Along with the directory structure, it creates a
+#' README file in Rmarkdown which can then be used to produce a GitHub
+#' compatible markdown document. It also creates a plot diary to help you
+#' organise plots generated during the project.
+#'
+#' @param pname a character, stating the name of the project
+#' @param path a character, specifying the location of the project
+#'
+#' @return NULL
+#'
+#' @details The README.Rmd file is created in the project directory and the
+#' plot_diary.Rmd created in the auto generated figures folder
+#'
+#' @export
+#'
+#' @seealso \code{\link{update_readme}}
+#'
+#' @examples
+#' \dontrun{
+#' # create project named "ds_project" in home folder
+#' createDSProject('ds_project', '~')
+#' }
+createDSProject <- function(pname = NULL, path = '.') {
+  stopifnot(!is.null(pname))
+
+  #create project directory
+  dpath = file.path(path, pname)
+  created = dir.create(dpath)
+  if (created) {
+    message(paste('project directory created:', dpath))
+  } else{
+    warning('directory exists, proceeding with setup')
+  }
+
+  #create subsequent directories
+  dir.create(file.path(dpath, '.dsproject'))                    #.dsproject
+  dir.create(file.path(dpath, 'literature'))                    #literature
+  dir.create(file.path(dpath, 'data'))                          #data
+  dir.create(file.path(dpath, 'code'))                          #code
+  dir.create(file.path(dpath, 'code', 'raw_code'))              #code/raw_code
+  dir.create(file.path(dpath, 'code', 'final_code'))            #code/final_code
+  dir.create(file.path(dpath, 'figures'))                       #figures
+  dir.create(file.path(dpath, 'figures', 'exploratory'))        #figures/exploratory
+  dir.create(file.path(dpath, 'figures', 'explanatory'))        #figures/explanatory
+  dir.create(file.path(dpath, 'products'))                      #products
+
+  #create Rmds for README
+  readmef = file.path(dpath, 'README.Rmd')
+  rmarkdown::draft(readmef, template = 'readme', package = 'restools', edit = FALSE)
+  rmarkdown::render(readmef, quiet = TRUE)
+
+  #create Rmds for plot_diary
+  pltdiaryf = file.path(dpath, 'figures', 'plot_diary.Rmd')
+  rmarkdown::draft(pltdiaryf, template = 'plot_diary', package = 'restools', edit = FALSE)
+  rmarkdown::render(pltdiaryf, quiet = TRUE)
+
+  #create Rmds for lit_review
+  litreviewf = file.path(dpath, 'literature', 'lit_review.Rmd')
+  rmarkdown::draft(litreviewf, template = 'lit_review', package = 'restools', edit = FALSE)
+  rmarkdown::render(litreviewf, quiet = TRUE)
+}
+
+init_git <- function() {
+  #initialise a git repo for the code folder
+}
+
+init_config <- function() {
+
+}
---
+++ R/update_readme.R
@@ -0,0 +1,122 @@
+#' Update the README file for the project
+#'
+#' Scans the project directory provided and creates entries for files in the
+#' README.Rmd file. Descriptions of files can then be modified manually.
+#'
+#' @param dsproj a character, providing the path of the project where the file
+#' needs to be updated.
+#'
+#' @return NULL
+#'
+#' @details Only the description of files/directories should be modified in the
+#' README.Rmd file. Modifying the comment tags or adding/removing text from the
+#' file names will result in their removal in the next update.
+#'
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # for "ds_project" in home folder
+#' update_readme('~/ds_project')
+#' }
+update_readme <- function(dsproj = '.') {
+  #read README file
+  rmdfile = file.path(dsproj, 'README.Rmd')
+  readme = readLines(rmdfile)
+
+  #update main sections
+  readme = updateReadmeSection(dsproj, readme, 'data')
+  readme = updateReadmeSection(dsproj, readme, 'code', 'raw_code')
+  readme = updateReadmeSection(dsproj, readme, 'code', 'final_code')
+  readme = updateReadmeSection(dsproj, readme, 'products')
+
+  writeLines(readme, con = rmdfile)
+  rmarkdown::render(rmdfile, quiet = TRUE)
+}
+
+#convert each file to a markdown record
+updateReadmeSection <- function(dsproj = '.', readme,  ...) {
+  #create record templates
+  dirpath = file.path(...)
+  files = getFiles(dsproj, dirpath)
+  frecords = paste0('* _', files, '_ - description here')
+  names(frecords) = files
+
+  #create tags
+  btag = paste0('<!--@', dirpath, ':begin-->') #begin
+  etag = paste0('<!--@', dirpath, ':end-->') #end
+  bpos = which(grepl(btag, readme))
+  epos = which(grepl(etag, readme))
+
+  #error if either position is missing
+  if (length(bpos) != 1 || length(epos) != 1) {
+    stop("Tags may have been modified, make sure only descriptions have been changed")
+  }
+
+  #extract relevant section and locate records
+  dirrecord = readme[seq(bpos, epos)]
+  dirrecord = readme[bpos + which(grepl('^*.*_.*_.*-', dirrecord)) - 1]
+  dirrecord = plyr::ldply(stringr::str_split(dirrecord, '-'))
+  frecords =  plyr::ldply(stringr::str_split(frecords, '-'))
+
+  #if no files, remove the record from the README
+  if (grepl('^*.*__', frecords[1, 1])) {
+    #replace original README section
+    readme = c(
+      readme[1:bpos],
+      "",
+      readme[epos:length(readme)]
+    )
+
+    return(readme)
+  }
+
+  #select information from old README where available
+  if (nrow(dirrecord) > 0){
+    colnames(frecords) = colnames(dirrecord) = c('file', 'desc')
+    rownames(dirrecord) = dirrecord$file
+    rownames(frecords) = frecords$file
+    commonrecs = intersect(dirrecord$file, frecords$file)
+    frecords[commonrecs, 'desc'] = dirrecord[commonrecs, 'desc']
+  }
+  frecords = apply(frecords, 1, paste, collapse = '-')
+  names(frecords) = NULL
+
+  #replace original README section
+  readme = c(
+    readme[1:bpos],
+    "",
+    frecords,
+    "",
+    readme[epos:length(readme)]
+  )
+
+  return(readme)
+}
+
+#get files within the folder of interest
+getFiles <- function(dsproj = '.',  ...) {
+  checkDSproject(dsproj)
+
+  qrypath = file.path(dsproj, ...)
+  files = list.files(qrypath, recursive = TRUE, include.dirs = TRUE, no.. = TRUE)
+
+  #return if no files
+  if (length(files) == 0)
+    return(character())
+
+  files = paste0('./', file.path(..., files))
+  files = sort(files)
+
+  return(files)
+}
+
+#stop if not a DS project
+checkDSproject <- function(dsproj = '.') {
+  stopifnot(isDSproject(dsproj))
+}
+
+#check if its a valid DS project
+isDSproject <- function(dsproj = '.') {
+  return(dir.exists(file.path(dsproj, '.dsproject')))
+}

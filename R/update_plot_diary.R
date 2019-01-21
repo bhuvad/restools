dbbcd1ab0d87f5f67bd6f376929121ca5f398fe0
bhuvad <bhuva.d@wehi.edu.au>
2019-01-21 20:52:59 +1100

    update templates to allow parsing of plot_diary

--- R/update_plot_diary.R
+++ R/update_plot_diary.R
@@ -1,1 +1,14 @@
 #' Update the plot_diary file for the project
+
+update_plot_diary <- function(dsproj = '.') {
+  #read README file
+  rmdfile = file.path(dsproj, 'figures/plot_diary.Rmd')
+  pltdiary = readLines(rmdfile)
+
+  #update main sections
+  readme = updateReadmeSection(dsproj, readme, 'code', 'raw_code')
+  readme = updateReadmeSection(dsproj, readme, 'code', 'final_code')
+
+  writeLines(readme, con = rmdfile)
+  rmarkdown::render(rmdfile, quiet = TRUE)
+}
